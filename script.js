ef66e38533675426b67f5ec540ff0d0d097dbaf2
SREEDHIL <sreedhilpavishanker@gmail.com>
2025-03-21 00:03:00 +0530

    updated

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
+const jwt = require("jsonwebtoken");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const SECRET_KEY = process.env.SECRET_KEY;
+
 const encrypt = (payload) => {
+  return jwt.sign(payload, SECRET_KEY, {expiresIn:"1h"});
   // encrypt the payload and return token
 }
 
 const decrypt = (token) => {
+  try {
+    return jwt.verify(token, SECRET_KEY);
+  } catch (error) {
+    console.error("Invalid or Expired token")
+  }
   // return decoded payload
 }
 
---
+++ test.js
@@ -0,0 +1,9 @@
+const {encrypt, decrypt} = require("./script");
+
+const payload = {userId: 123, role: "admin"};
+
+const token = encrypt(payload);
+console.log("Generated Token:",token);
+
+const decoded = decrypt(token);
+console.log("Decoded Token",decoded)
